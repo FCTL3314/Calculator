14970ea78a5078001b6ea7efeb80bf6ca9cd53a8
Nikita <Solovev.nikita.05@gmail.com>
2022-08-27 16:22:34 +0300

    Add an exception(ValueError)

--- Calculator/__init__.py
+++ Calculator/__init__.py
@@ -1,15 +1,18 @@
 
 
 def main():
-    user_choice = int(input('1. Plus\n2.Minus\n3.Multiply\n4.Divide\nChoose the desired action: '))
-    if user_choice == 1:
-        print(plus(float(input('Enter the first number: ')), float(input('Enter the second number: '))))
-    elif user_choice == 2:
-        print(minus(float(input('Enter the first number: ')), float(input('Enter the second number: '))))
-    elif user_choice == 3:
-        print(multiply(float(input('Enter the first number: ')), float(input('Enter the second number: '))))
-    elif user_choice == 4:
-        print(divide(float(input('Enter the first number: ')), float(input('Enter the second number: '))))
+    try:
+        user_choice = int(input('1. Plus\n2.Minus\n3.Multiply\n4.Divide\nChoose the desired action: '))
+        if user_choice == 1:
+            print(plus(float(input('Enter the first number: ')), float(input('Enter the second number: '))))
+        elif user_choice == 2:
+            print(minus(float(input('Enter the first number: ')), float(input('Enter the second number: '))))
+        elif user_choice == 3:
+            print(multiply(float(input('Enter the first number: ')), float(input('Enter the second number: '))))
+        elif user_choice == 4:
+            print(divide(float(input('Enter the first number: ')), float(input('Enter the second number: '))))
+    except ValueError:
+        print('You can only enter numbers')
 
 
 def plus(a, b):
